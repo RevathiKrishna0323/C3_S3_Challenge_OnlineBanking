6f6ad2031efbbc5de0dc5ae74fe2f4e9ba079663
Revathi G <revathikrishna0323@gmail.com>
2022-08-26 20:07:58 +0530

    added Source code in the Savings account impl class

--- src/main/java/com/niit/jap/SavingsAccount.java
+++ src/main/java/com/niit/jap/SavingsAccount.java
@@ -19,96 +19,99 @@ public class SavingsAccount {
     private double interestRate;
     private String modeOfOperation;
 
+    public SavingsAccount() {
+
+    }
+
     public String getAccountName() {
         return accountName;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getAccountOpeningDate() {
-        return accountOpeningDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public double getAvailableBalance() {
-        return availableBalance;
-    }
-
-    public double getTotalBalance() {
-        return totalBalance;
-    }
-
-    public String getInternetBanking() {
-        return InternetBankingStatus;
-    }
-
-    public String getMobileBanking() {
-        return MobileBankingStatus;
-    }
-
-    public double getInterestRate() {
-        return interestRate;
-    }
-
-    public String getModeOfOperation() {
-        return modeOfOperation;
     }
 
     public void setAccountName(String accountName) {
         this.accountName = accountName;
     }
 
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
+    }
+
+    public String getAccountOpeningDate() {
+        return accountOpeningDate;
     }
 
     public void setAccountOpeningDate(String accountOpeningDate) {
         this.accountOpeningDate = accountOpeningDate;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public double getAvailableBalance() {
+        return availableBalance;
     }
 
     public void setAvailableBalance(double availableBalance) {
         this.availableBalance = availableBalance;
     }
 
+    public double getTotalBalance() {
+        return totalBalance;
+    }
+
     public void setTotalBalance(double totalBalance) {
         this.totalBalance = totalBalance;
+    }
+
+    public String getInternetBanking() {
+        return InternetBankingStatus;
     }
 
     public void setInternetBanking(String internetBankingStatus) {
         InternetBankingStatus = internetBankingStatus;
     }
 
+    public String getMobileBanking() {
+        return MobileBankingStatus;
+    }
+
     public void setMobileBanking(String mobileBankingStatus) {
         MobileBankingStatus = mobileBankingStatus;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
     }
 
     public void setInterestRate(double interestRate) {
         this.interestRate = interestRate;
     }
 
+    public String getModeOfOperation() {
+        return modeOfOperation;
+    }
+
     public void setModeOfOperation(String modeOfOperation) {
         this.modeOfOperation = modeOfOperation;
     }
-    public SavingsAccount() {
 
-    }
-
-    public double DebitCash(double cash){
+    public double DebitCash(double cash) {
         return cash = this.availableBalance - cash;
     }
-    public double CreditCash(double cash){
+
+    public double CreditCash(double cash) {
         return cash = this.availableBalance + cash;
     }
-    public double RetrieveCash(double cash){
+
+    public double RetrieveCash(double cash) {
         return cash;
     }
 }
